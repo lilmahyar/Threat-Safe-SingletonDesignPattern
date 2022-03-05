ea2fa81bc55601363365aa905fbe0a1242e68891
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-03-05 13:42:58 +0330

    Add project files.

---
+++ Threat-Safe-SingletonDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Threat_Safe_SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Threat-Safe-SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threat_Safe_SingletonDesignPattern
+{
+    public class Singleton
+    {
+
+    }
+}
